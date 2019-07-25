00562c10e742ad967f9039381220bfa4e29e1bbf
Sebastian Basiński <basinski.sebastian@gmail.com>
2019-07-25 11:50:16 +0200

    Create basic files

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const UsersService = require('./UsersService');
+
+
+const usersService = new UsersService();
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('join', (name) => {
+	  usersService.addUser({
+	    id: socket.id,
+	    name
+	  });
+	  io.emit('update', {
+	    users: usersService.getAllUsers()
+	  });
+	});
+});
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    usersService.removeUser(socket.id);
+    socket.broadcast.emit('update', {
+      users: usersService.getAllUsers()
+    });
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    const {name} = usersService.getUserById(socket.id);
+    socket.broadcast.emit('message', {
+      text: message.text,
+      from: name
+    });
+  });
+});
+
+server.listen(3000, () => {
+	console.log('Listening on :3000');
+});
+
